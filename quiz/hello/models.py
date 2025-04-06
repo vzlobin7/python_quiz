e71f4ee5056962ae64d87f13962d9b12bbb02f4e
vzlobin7 <vzlobin74@gmail.com>
2025-04-06 16:56:08 +0300

    Followed to https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Server-side/Django/Admin_site

--- quiz/hello/models.py
+++ quiz/hello/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import IntegerField
+
 
 # Create your models here.
+
+class MyQuiz(models.Model):
+    name = models.CharField(max_length=256, help_text='Enter a quiz name')
+    def __str__(self):
+        return self.name
+
+class QuizQuestion(models.Model):
+    text_question = models.CharField(max_length=1024)
+    image_question = models.ImageField()
+    option1 = models.CharField(max_length=64)
+    option2 = models.CharField(max_length=64)
+    option3 = models.CharField(max_length=64)
+    option4 = models.CharField(max_length=64)
+    correct_option = IntegerField()
+    quiz = models.ForeignKey('MyQuiz', on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.text_question
