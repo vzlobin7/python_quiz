c4887e374d2d6b20de283b612da520c7dde2284c
vzlobin7 <vzlobin74@gmail.com>
2025-04-23 18:11:19 +0300

    Processed button pushing

--- quiz/hello/views.py
+++ quiz/hello/views.py
@@ -12,6 +12,7 @@ def question(request, id):
         request,
         'question.html',
         context={'question':q.text_question,'image':q.image_question,
-                 'option1':q.option1,'option2':q.option2,'option3':q.option3,'option4':q.option4},
+                 'option1':q.option1,'option2':q.option2,'option3':q.option3,'option4':q.option4,
+                 'correct_option':q.correct_option, 'question_count': QuizQuestion.objects.all().count()},
     )
 
